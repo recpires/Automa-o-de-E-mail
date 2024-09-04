141e47fef2b37943f973417038deaf66604b516e
Rodrigo Pires <rec.pires7@gmail.com>
2024-09-04 11:10:34 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,59 @@
+let dados = [
+{
+    titulo: "Triumph Tiguer 800xrx",
+    descricao: "A Triumph Tiger 800 XRX é uma motocicleta adventure de média cilindrada que combina desempenho, conforto e versatilidade.Com seu motor tricilíndrico de 800cc, \
+    a Tiger 800 XRX oferece uma entrega de potência suave e linear, ideal para longas jornadas. Seu chassi robusto e suspensão ajustável garantem estabilidade em diferentes tipos de terreno, \
+    enquanto os pneus de uso misto proporcionam aderência tanto no asfalto quanto em trilhas leves. Além disso, a Tiger 800XRX vem equipada com uma série de recursos eletrônicos, \
+    como modos de pilotagem, controle de tração e ABS, que aumentam a segurança e o prazer ao pilotar. Se você busca uma moto para explorar \
+    diferentes destinos e vivenciar aventuras inesquecíveis, a Triumph Tiger 800 XRX é uma excelente opção,",
+    link: "https://www.youtube.com/watch?v=-FAn9QoQ2tw",
+}, 
+
+{
+    titulo: "Harley Davidson Fat Boy 114",
+    descricao: "A Harley Davidson Fat Boy 114 é um ícone do motociclismo americano. Com seu motor Milwaukee-Eight 114, oferece um ronco inconfundível \
+    e um torque poderoso. Seu design clássico, com rodas de liga leve e pintura personalizada, a torna uma motocicleta única e desejada. \
+    Ideal para quem busca estilo e performance",
+    link: "https://www.youtube.com/watch?v=FT_kdpWTnLs",
+},
+
+{
+    titulo: "BMW R 1300 GS",
+    descricao: "A BMW R1300 GS é uma referência em motocicletas adventure. Com seu potente motor boxer de 1300cc, oferece um torque impressionante \
+    e uma experiência de pilotagem única. Seu design robusto e suspensão eletrônica adaptativa garantem conforto e segurança em qualquer terreno. Ideal para longas \
+    viagens e aventuras off-road",
+    link: "https://www.youtube.com/watch?v=Ukl07MGzzVA",
+},
+
+{
+    titulo: "Triumph Scrambler 400X",
+    descricao: "A Triumph Scrambler 400X é uma moto clássica com um toque moderno, perfeita para aventuras urbanas e leves trilhas.\
+    Com seu motor de 399cc, ela oferece um equilíbrio entre desempenho e economia de combustível. Seu design retro e posição de pilotagem confortável\
+    a tornam uma ótima opção para quem busca uma moto estilosa e versátil.",
+    link: "https://www.youtube.com/watch?v=huuq4Jb5oV0",
+}, 
+
+{
+    titulo: "BMW R1250 GS Adventure",
+    descricao: "A BMW R1250 GS Adventure é uma maxitrail robusta e aventureira, projetada para enfrentar qualquer desafio.\
+    Com seu potente motor boxer de 1250cc e suspensão eletrônica semi-ativa, ela oferece conforto e desempenho em qualquer terreno.\
+    Ideal para longas expedições e viagens off-road.",
+    link: "https://www.youtube.com/watch?v=elsopcZQaSo",
+},
+
+{
+    titulo: "Honda Shadow 750",
+    descricao: "A Honda Shadow 750 é uma cruiser clássica, perfeita para quem busca um estilo atemporal e um passeio relaxante.\
+    Com seu motor V-twin de 750cc e um design elegante, ela oferece uma experiência de pilotagem suave e confortável.",
+    link: "https://www.youtube.com/watch?v=uKuZNNamIn4&pp=ygUQSG9uZGEgU2hhZG93IDc1MA%3D%3D",
+},
+
+{
+    titulo: "Suzuki Boulevard 800M",
+    descricao: "A Suzuki Boulevard 800M é uma cruiser moderna com um visual imponente e um desempenho emocionante.\
+    Seu motor V-twin de 800cc e um design musculoso proporcionam uma pilotagem ágil e divertida.",
+    link: "https://www.youtube.com/watch?v=d1BXAuZzipQ&pp=ygUOYm91bGV2YXJkIG04MDA%3D",
+},
+
+];
+
