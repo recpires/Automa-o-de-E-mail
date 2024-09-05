f2ff962803613f2f4f57eb8ec44483a88508035a
Rodrigo Pires <rec.pires7@gmail.com>
2024-09-05 10:16:01 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log(dados);
+function pesquisar() {
+    // Seleciona a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    // Inicializa uma string vazia para armazenar o HTML dos resultados
+    let resultados = "";
+
+    // Itera sobre cada dado da lista de dados
+    for (let dado of dados) {
+        // Cria o HTML de um item de resultado
+        resultados += `
+            <div class="item-resultado">
+                <img src="${dado.imagem}">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">mais informações</a>
+            </div>
+        `;
+    }
+
+    // Atualiza o conteúdo da seção com os resultados gerados
+    section.innerHTML = resultados;
+}
+// console.log(dados);
