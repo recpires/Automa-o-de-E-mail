2c2d7436263a723204c9e0cf57ffc26648462dba
Rodrigo Pires <rec.pires7@gmail.com>
2024-09-06 13:21:39 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,13 +2,30 @@ function pesquisar() {
     // Seleciona a seção onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    // Se campoPesquisa for uma string sem nada
+    if(!campoPesquisa) {
+        section.innerHTML = "Digite um Modelo Válido"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+ 
     // Inicializa uma string vazia para armazenar o HTML dos resultados
     let resultados = "";
-
+    let titulo = "";
+    let descricao = "";
+    let tags  = "";
     // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
-        // Cria o HTML de um item de resultado
-        resultados += `
+        titulo = dado.titulo.toLocaleLowerCase()
+        descricao = dado.descricao.toLocaleLowerCase()
+        tags = dado.tags.toLocaleLowerCase()
+        // se titulo includes campoPesquisa
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            // Cria o HTML de um item de resultado
+            resultados += `
             <div class="item-resultado">
                 <img src="${dado.imagem}">
                 <h2>
@@ -18,8 +35,13 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank">mais informações</a>
             </div>
         `;
-    }
+        }
+     
 
+    }
+    if(!resultados) {
+        resultados = "<p>Modelo não Encontrado</p>"
+    }
     // Atualiza o conteúdo da seção com os resultados gerados
     section.innerHTML = resultados;
 }
